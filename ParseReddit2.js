8ead5233e79da78cf8bd97757ef5ca2232e4a957
Aedan Dispenza <adispen@Aedans-Mac.local>
2013-03-31 03:13:42 -0400

    fixed random apostrophes

--- ParseReddit2.js
+++ ParseReddit2.js
@@ -7,17 +7,17 @@ jsdom.env({
 		var $ = window.$;
 		var stories = $.map($('#siteTable .thing:lt(1)'), function (thing) {
 			stories.title1 = $('a.title', thing).text(),
-			stories.href1= $('a.title',thing).attr('href')'
+			stories.href1= $('a.title',thing).attr('href')
 			stories.permalink1= $('a.comments', thing).attr('href'),
 		};
 		var stories = $.map($('#siteTable .thing:lt(2)'), function (thing) {
 			stories.title2 = $('a.title', thing).text(),
-			stories.href2= $('a.title',thing).attr('href')'
+			stories.href2= $('a.title',thing).attr('href')
 			stories.permalink2= $('a.comments', thing).attr('href'),
 		};
 		var stories = $.map($('#siteTable .thing:lt(3)'), function (thing) {
 			stories.title3 = $('a.title', thing).text(),
-			stories.href3= $('a.title',thing).attr('href')'
+			stories.href3= $('a.title',thing).attr('href')
 			stories.permalink3= $('a.comments', thing).attr('href'),
 		};
 	});
